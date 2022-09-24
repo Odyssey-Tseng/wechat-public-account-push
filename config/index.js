d82f5192867979447bd7a2352caecfc32768d88f
Odyssey-Tseng <52033953+Odyssey-Tseng@users.noreply.github.com>
2022-09-24 21:40:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -68,7 +68,7 @@ export const USER_CONFIG = {
       // 所在省份
       province: "湖北",
       // 所在城市
-      city: "洪山",
+      city: "武汉",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -78,8 +78,8 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+       // {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+       // {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "周同学", "year": "1998", "date": "06-27"},
         {"type": "节日", "name": "结婚纪念日", "year": "2021", "date": "05-20"},
@@ -87,11 +87,11 @@ export const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
+        {"keyword": "love_day", date: "2016-11-01"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2021-05-20"},
         // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+       // {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
     {
