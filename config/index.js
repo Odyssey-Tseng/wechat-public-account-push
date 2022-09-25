70e9055c69e9e367fddd07c6f89589ee802fe0b9
Odyssey-Tseng <52033953+Odyssey-Tseng@users.noreply.github.com>
2022-09-25 19:05:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "周同学",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oNFZF6laN0BA43UVC3Ls_CRksaAM",
+      id: "oNFZF6tOrZOiKkSWUTiA5d49v2us",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "0lRzJMu31NlRylqgV99GdKM4jj6_fIKzjo9TQOf6nMQ",
+      useTemplateId: "U5VIMocDVyMhlFHWbNv-wmhzRPCwD8Ga8Xa-KUK8RT0",
       // 所在省份
       province: "湖北",
       // 所在城市
@@ -85,6 +85,8 @@ export const USER_CONFIG = {
         {"type": "生日", "name": "曾同学", "year": "1995", "date": "07-15"},
         {"type": "节日", "name": "结婚纪念日", "year": "2021", "date": "05-20"},
         {"type": "节日", "name": "下一次见面", "year": "2022", "date": "10-16"},
+        {"type": "节日", "name": "国家公务员考试", "year": "2022", "date": "11-26"},
+        {"type": "节日", "name": "浙江选调", "year": "2022", "date": "10-29"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
@@ -96,20 +98,38 @@ export const USER_CONFIG = {
         //{"keyword": "ex_day", date: "2022-10-16"},
       ]
     },
-   /*
+ 
    {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
+      name: "曾同学",
+      id: "oNFZF6laN0BA43UVC3Ls_CRksaAM",
+      useTemplateId: "U5VIMocDVyMhlFHWbNv-wmhzRPCwD8Ga8Xa-KUK8RT0",
+      province: "浙江",
+      city: "义乌",
       horoscopeDate: '',
       horoscopeDateType: '',
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '12-27',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://weibo.com/peanut013",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+       // {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+       // {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "周同学", "year": "1998", "date": "06-27"},
+        {"type": "生日", "name": "曾同学", "year": "1995", "date": "07-15"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2021", "date": "05-20"},
+        {"type": "节日", "name": "下一次见面", "year": "2022", "date": "10-16"},
+        {"type": "节日", "name": "国家公务员考试", "year": "2022", "date": "11-26"},
+        {"type": "节日", "name": "浙江选调", "year": "2022", "date": "10-29"},
+      ],
       customizedDateList: []
     },
-    */
+  
     // 你可以不断按格式往下增加
     // ...
   ],
